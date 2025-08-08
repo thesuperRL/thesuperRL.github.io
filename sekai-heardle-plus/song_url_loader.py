f9dad0f8aab77b2d41b60364fd032d38fa679308
thesuperRL <ryanli1366@gmail.com>
2025-08-08 16:29:07 +0800

    urls

--- sekai-heardle-plus/song_url_loader.py
+++ sekai-heardle-plus/song_url_loader.py
@@ -22,7 +22,7 @@ try:
         row = songs_url[i]
         row["Unit"] = songs[i]["Unit"]
         row["Singers"] = songs[i]["Singers"]
-        if (row['URL'] == ""):
+        if (row['URL'] == "" and row["Unit"] != "VIRTUAL SINGER"):
             row["URL"] = input(row["Song title"] + " " + str(row["Singers"]) + " " + row["Producer"] + ">> ")
         concat.append(row)
 
@@ -31,10 +31,3 @@ try:
         json.dump(concat, file, indent=4, ensure_ascii=False)
 except Exception as e:
     print(f"Failed to read JSON file: {e}")
-
-# *Hello, Planet. ['Hatsune Miku']>> https://soundcloud.com/dany-rimache-auqui/hatsune-miku-hello-planet
-# World's End Dancehall ['Hatsune Miku', 'Megurine Luka']>> https://soundcloud.com/shadstrike/worlds-end-dancehall
-# Romeo and Cinderella ['Hatsune Miku', 'Momoi Airi', 'Hinomori Shizuku']>> https://soundcloud.com/loveclubs/romeo-and-cinderella-shizuku-airi-more-more-jump-from-project-sekai
-# Miku Miku ni Shite Ageru ♪ 【Shiteyanyo】 ['Hatsune Miku']>> https://soundcloud.com/bloggermicro/miku-miku-ni-shite-ageru
-# Brand New Day ['Hatsune Miku']>> https://soundcloud.com/irucaice/brandnewday
-# Hatsune Miku no Gekishou ['Hatsune Miku']>>
